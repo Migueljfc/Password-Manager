4fe42687ba64d851fbd91bfc452e57c566543546
Miguel Cabral <44329689+Migueljfc@users.noreply.github.com>
2019-12-05 20:26:20 +0000

    Creation of new employees

--- passwords.py
+++ passwords.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
+
 import sqlite3
 
 MATER_PASSWORD = "1234"
-senha = input("Insira a senha master:")
+senha = input("Enter the master password: ")
 if senha != MATER_PASSWORD:
-    print("ERRO:Senha inválida!")
+    print("ERROR:Invalid password!")
     exit()
 
 conn = sqlite3.connect('passwords.db')
@@ -22,17 +22,23 @@ def menu():
     print(" _______________________________")
     print("|      Passwords Manager        |")
     print("|_______________________________|")
-    print("| 1-> Inserir senha nova        |")
-    print("| 2-> Listar serviços guardados |")
-    print("| 3-> Recuperar uma senha       |")
-    print("| 0-> Sair                      |")
+    print("| 1-> Enter new password        |")
+    print("| 2-> List saved services       |")
+    print("| 3-> Recover a password        |")
+    print("| 4-> Delete user               |")
+    print("| 5-> Update password           |")
+    print("| 0-> Exit                      |")
     print("|_______________________________|")
+
 def show_services():
     cursor.execute('''
         SELECT service FROM users;
         ''')
-    for service in cursor.fetchall():
-        print(service)
+    if cursor.rowcount == 0:
+        print("Empty")
+    else:
+        for service in cursor.fetchall():
+            print(service)
 
 def insert_password(service,username,password):
     cursor.execute(f'''
@@ -42,22 +48,61 @@ def insert_password(service,username,password):
     conn.commit()
 
 def get_password(service):
-    pass  #falta acabar 
+    cursor.execute(f'''
+        SELECT username,password FROM users
+        WHERE service = '{service}'
+    ''')
+    if cursor.rowcount == 0:
+        print("Service not found, please register it first.")
+    else:
+        for user in cursor.fetchall():
+            print(user)
 
+def del_user(username):
+    cursor.execute(f'''
+        DELETE FROM users WHERE username = '{username}'
+    ''')
+    if cursor.rowcount == 0:
+        print("User not found!")
+    else:
+        print("Deleted")
+    conn.commit()
+
+def edit(username,password,service):
+    cursor.execute(f'''
+        UPDATE users SET password = '{password}' WHERE username = '{username}' AND service = '{service}'
+    ''')
+    if cursor.rowcount == 0:
+        print('User not found')
+    else :
+        print("Success")
+    conn.commit()
 while True:
     menu()
-    op = input("Opção->")
-    if op not in['0','1','2','3']:
-        print("Opção inválida")
+    op = input("Option-> ")
+    if op not in['0','1','2','3','4','5']:
+        print("Invalid Option")
         continue
     if op == '0' :
         break
     if op == '1':
-        service = input('Qual o nome do serviço? ')
-        username = input('Qual o nome do utilizador? ')
-        password = input('Qual a sua password? ')
+        service = input('What\'s the name of the service ? ')
+        username = input('What\'s the user\'s name? ')
+        password = input('What\'s your password?')
         insert_password(service, username, password)
     if op == '2':
         show_services()
-    
+    if op == '3':
+        service = input('What service do you want the password for ?  ')
+        get_password(service)
+    if op == '4':
+        username = input('What\'s the user\'s name? ')
+        del_user(username)
+    if op == '5':
+        username = input('What\'s the user\'s name ? ')
+        service = input ('What\'s the service ? ')
+        password = input('What is the new password ?')
+        edit(username,password,service)
+
+
 conn.close()
